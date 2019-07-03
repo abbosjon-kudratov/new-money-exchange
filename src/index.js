ca7b4745b7533bed06c108f464a34eb0d721c7eb
abbosjon-kudratov <abbosjon.kudratov@gmail.com>
2019-07-03 15:23:09 +0500

    function not working version ready!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
     // Your code goes here!
+    
     // Return an object containing the minimum number of coins needed to make change
+
+    // var H=0;
+    // var Q=0;
+    // var rem=100
+
+    var cur=currency;
+    var obj={
+    var H=cur%50;
+     // if (H) {}
+    var Q=cur%20;
+     var D=cur%10;
+     var N=cur%5;
+     var P=cur%1;}
+     return obj;
+     // return('hello!');
 }
