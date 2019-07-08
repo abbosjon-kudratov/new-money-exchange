89a205dad3630c6218d063da715e05e1a02d5e54
abbosjon-kudratov <abbosjon.kudratov@gmail.com>
2019-07-08 10:26:41 +0500

    done, but copied from someone else

--- src/index.js
+++ src/index.js
@@ -7,31 +7,29 @@ module.exports = function makeExchange(currency) {
     // var H=0;
     // var Q=0;
     // var rem=100
-    var cur=currency;
-    var obj={
-            H: Math.floor(cur/50),
-                // cur = cur - Math.floor(cur/50)*50;
-            Q: Math.floor((cur%50)/20),
-            D: Math.floor((cur%20)/10),
-            N: Math.floor((cur%10)/5),
-            P: Math.floor((cur%5)/1),
-            error: "You are rich, my friend! We don't have so much coins for exchange"
-        };
+    if (currency==0) return{};
+    if (currency >= 10000)
+    return { error: "You are rich, my friend! We don't have so much coins for exchange" };
 
-        if (obj.H===0) {delete obj.H;}
-        if (obj.Q===0) {delete obj.Q;}
-        if (obj.D===0) {delete obj.D;}
-        if (obj.N===0) {delete obj.N;}
-        if (obj.P===0) {delete obj.P;}
-    
-    
-        var obj2={};
+    const result = {};
+    const config = {
+            H: 50,
+            Q: 25,
+            D: 10,
+            N: 5,
+            P: 1
+          };
 
-        if (currency==0) { var obj1={}; return obj1;}
-        if (currency>10000) {var obj1={error: "You are rich, my friend! We don't have so much coins for exchange"};
-                                return obj1;}
-     return obj;
-     // return('hello!');
+        for (const par in config) {
+        if (config.hasOwnProperty(par)) {
+          if (Math.floor(currency / config[par]) !== 0) {
+            result[par] = Math.floor(currency / config[par]);
+            currency = currency % config[par];
+          }
+        }
+      }
+
+      return result;
 }
 
 
